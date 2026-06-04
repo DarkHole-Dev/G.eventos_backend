16002bfed4b21704e289ac8d65bd6466310b938e
AKIRA_NZR <dkaua0955@gmail.com>
2026-06-04 10:01:26 -0300

    Estrutura inicial

---
+++ src/main/java/com/darkhole/gerenteEventos/GerenteEventosApplication.java
@@ -0,0 +1,13 @@
+package com.darkhole.gerenteEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenteEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenteEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darkhole/gerenteEventos/GerenteEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkhole.gerenteEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenteEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
